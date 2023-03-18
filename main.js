b2dd4c2300ecf71baaf83e0fb849ffbd1208d3a6
Dimash Turgan <turgan.dimash@mail.ru>
2023-03-18 09:53:24 +0600

    Midterm project Dimash and Azamat

---
+++ main.js
@@ -0,0 +1,7 @@
+var navToggle = document.querySelector('.nav-toggle');
+var navLinks = document.querySelector('.nav-links');
+
+	navToggle.addEventListener('click', function() {
+		navLinks.classList.toggle('open');
+	});
+ 
